2042574608844d064c0929f8bd7f69e96108f963
Kian Seyedjafari <kian.matthew@berkeley.edu>
2023-04-19 09:16:09 -0700

    fixed redux state when editing a review for stars component

--- react-app/src/store/review.js
+++ react-app/src/store/review.js
@@ -181,7 +181,7 @@ let initialState = {
             // console.log("Still has old review", copyReviews)
             copyReviews[action.payload.id] = action.payload
             // console.log("changed Review", copyReviews)
-            return { ...state, UserReviews: copyReviews }
+            return { ...state, UserReviews: copyReviews, LocationReviews: {} }
         case DELETE_REVIEW:
             newState = {...state}
             let reviewCopy = {...newState.UserReviews}

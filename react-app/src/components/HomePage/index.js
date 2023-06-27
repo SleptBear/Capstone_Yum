cd5c2b5d4eddfc5d04d85148032e4ab81e92bbbc
Kian Seyedjafari <kian.matthew@berkeley.edu>
2023-06-27 13:51:20 -0700

    background image refactored with css

--- react-app/src/components/HomePage/index.js
+++ react-app/src/components/HomePage/index.js
@@ -26,11 +26,11 @@ function HomePage() {
         <div className="Home">
             <div className="background">
                 {/* <img src="https://inkind.com/images/tild3033-3634-4136-b937-373235383063__foodbanner-wide.jpg" alt="Not Found"> */}
-                <img src="https://www.lyledc.com/media/pjbkwhfy/lyle-hotel-1914-web-wide.jpg?anchor=center&mode=crop&quality=70&width=1280&height=647&rnd=133057541708130000" alt="Not Found">
+                {/* <img src="https://www.lyledc.com/media/pjbkwhfy/lyle-hotel-1914-web-wide.jpg?anchor=center&mode=crop&quality=70&width=1280&height=647&rnd=133057541708130000" alt="Not Found"> */}
                 {/* <img src="https://www.brandinginasia.com/wp-content/uploads/2021/06/Dark-Kitchen-Dipo-Induction.jpg" alt="Not Found"> */}
                 {/* <img src="https://images.squarespace-cdn.com/content/v1/5b4d0a8f2487fd811172f260/1572886853604-1PSXIC5U0UZUU0JM4S5F/f8.JPG" alt="Not Found"> */}
                 {/* <img src="https://cf.bstatic.com/images/hotel/max1024x768/197/197749692.jpg" alt="Not Found"> */}
-                </img>
+                {/* </img> */}
             </div>
             <div className="recent-activity">
             <h1>
